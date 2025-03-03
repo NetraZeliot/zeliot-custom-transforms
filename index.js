783b7fc0b94d443a58a1da213e495094d6bcfba1
Netra <netravati@zeliot.in>
2025-03-03 21:49:56 +0530

    harsh-acceleration

--- index.js
+++ index.js
@@ -49,12 +49,12 @@ const run = async () => {
           if (redisValue) {
             console.log(`Unique ID ${vehicle} exists in Redis with value: ${redisValue}`);
   
-            if (redisValue === "ignition_on") {
-              console.log(`Key '${redisKey}' has value 'ignition_on'. No action needed.`);
+            if (redisValue === "harsh_acceleration_start") {
+              console.log(`Key '${redisKey}' has value 'harsh_acceleration_start'. No action needed.`);
             } else {
-              let messageToPublish = JSON.stringify({ imei: vehicle, status: "ignition_off" });
+              let messageToPublish = JSON.stringify({ imei: vehicle, status: "harsh_acceleration_stop" });
               await producer.send({
-                topic: "output",
+                topic: process.env.KAFKA_PRODUCER_TOPIC,
                 messages: [{ value: messageToPublish }],
               });
   
@@ -64,17 +64,15 @@ const run = async () => {
           } else {
             console.log(`Unique ID ${vehicle} does not exist in Redis.`);
   
-            if ((eventFlag & 1024) === 1024) {
-              await writeToRedisWithKeyValue(redisKey, "ignition_on");
-              console.log(`Stored IMEI ${vehicle} with value 'ignition_on' in Redis.`);
+            if ((eventFlag & 67108864) === 67108864) {
+              await writeToRedisWithKeyValue(redisKey, "harsh_acceleration_start");
+              console.log(`Stored IMEI ${vehicle} with value 'harsh_acceleration_start' in Redis.`);
   
-              let messageToPublish = JSON.stringify({ imei: vehicle, status: "ignition_on" });
+              let messageToPublish = JSON.stringify({ imei: vehicle, status: "harsh_acceleration_start" });
               await producer.send({
-                topic: "output",
+                topic: process.env.KAFKA_PRODUCER_TOPIC,
                 messages: [{ value: messageToPublish }],
               });
-            } else if ((eventFlag & 4096) === 4096) {
-              console.log(`Key '${redisKey}' has value 'ignition_off'. No action needed.`);
             } else {
               console.log(`Unexpected event_flag value: ${eventFlag} for unique ID ${vehicle}`);
             }
