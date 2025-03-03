fc59ba59b030a509082c59d36a61c7e909004b3f
Netra <netravati@zeliot.in>
2025-03-03 21:29:51 +0530

    harsh-cornering

--- index.js
+++ index.js
@@ -41,20 +41,18 @@ const run = async () => {
         let eventFlag = input.event_flag;
   
         let redisKey = `imei_${vehicle}`;
-  
-          // console.log(`Processing message for Unique ID: ${uniqueId}`);
-  
+    
           let redisValue = await getKeyValueSentinel(redisKey);
   
           if (redisValue) {
             console.log(`Unique ID ${vehicle} exists in Redis with value: ${redisValue}`);
   
-            if (redisValue === "ignition_on") {
-              console.log(`Key '${redisKey}' has value 'ignition_on'. No action needed.`);
+            if (redisValue === "harsh_cornering_start") {
+              console.log(`Key '${redisKey}' has value 'harsh_cornering_start'. No action needed.`);
             } else {
-              let messageToPublish = JSON.stringify({ imei: vehicle, status: "ignition_off" });
+              let messageToPublish = JSON.stringify({ imei: vehicle, status: "harsh_cornering_stop" });
               await producer.send({
-                topic: "output",
+                topic: process.env.KAFKA_PRODUCER_TOPIC,
                 messages: [{ value: messageToPublish }],
               });
   
@@ -64,17 +62,16 @@ const run = async () => {
           } else {
             console.log(`Unique ID ${vehicle} does not exist in Redis.`);
   
-            if ((eventFlag & 1024) === 1024) {
-              await writeToRedisWithKeyValue(redisKey, "ignition_on");
-              console.log(`Stored IMEI ${vehicle} with value 'ignition_on' in Redis.`);
+            if ((eventFlag & 268435456) === 268435456) {
+              await writeToRedisWithKeyValue(redisKey, "harsh_cornering_start");
+              console.log(`Stored IMEI ${vehicle} with value 'harsh_cornering_start' in Redis.`);
   
-              let messageToPublish = JSON.stringify({ imei: vehicle, status: "ignition_on" });
+              let messageToPublish = JSON.stringify({ imei: vehicle, status: "harsh_cornering_start" });
               await producer.send({
-                topic: "output",
+                topic: process.env.KAFKA_PRODUCER_TOPIC,
                 messages: [{ value: messageToPublish }],
               });
-            } else if ((eventFlag & 4096) === 4096) {
-              console.log(`Key '${redisKey}' has value 'ignition_off'. No action needed.`);
+          
             } else {
               console.log(`Unexpected event_flag value: ${eventFlag} for unique ID ${vehicle}`);
             }
