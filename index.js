0d53f078bbb2fbc0119fc58b56af2eb5cc65668b
NetraZeliot <netravati@zeliot.in>
2025-03-04 16:16:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,53 +32,16 @@ const run = async () => {
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       try{
-        console.log("This is message",message.value.toString())
-        let input = JSON.parse(message.value.toString());
-        console.log("THis is input...",input)
-        let vehicle = input.uniqueId;
-        console.log(`Vehicle Unique ID = ${vehicle}`);
-  
-        let eventFlag = input.event_flag;
-  
-        let redisKey = `imei_${vehicle}`;
-  
-          // console.log(`Processing message for Unique ID: ${uniqueId}`);
-  
-          let redisValue = await getKeyValueSentinel(redisKey);
-  
-          if (redisValue) {
-            console.log(`Unique ID ${vehicle} exists in Redis with value: ${redisValue}`);
-  
-            if (redisValue === "ignition_on") {
-              console.log(`Key '${redisKey}' has value 'ignition_on'. No action needed.`);
-            } else {
-              let messageToPublish = JSON.stringify({ imei: vehicle, status: "ignition_off" });
-              await producer.send({
-                topic: "output",
-                messages: [{ value: messageToPublish }],
-              });
-  
-              await deleteFromRedis(redisKey);
-              console.log(`Removed key '${redisKey}' from Redis.`);
-            }
-          } else {
-            console.log(`Unique ID ${vehicle} does not exist in Redis.`);
-  
-            if ((eventFlag & 1024) === 1024) {
-              await writeToRedisWithKeyValue(redisKey, "ignition_on");
-              console.log(`Stored IMEI ${vehicle} with value 'ignition_on' in Redis.`);
-  
-              let messageToPublish = JSON.stringify({ imei: vehicle, status: "ignition_on" });
-              await producer.send({
-                topic: "output",
-                messages: [{ value: messageToPublish }],
-              });
-            } else if ((eventFlag & 4096) === 4096) {
-              console.log(`Key '${redisKey}' has value 'ignition_off'. No action needed.`);
-            } else {
-              console.log(`Unexpected event_flag value: ${eventFlag} for unique ID ${vehicle}`);
-            }
-          }
+        const msgValue = message.value.toString();
+        console.log("Received message:", msgValue);
+
+        // Publish the same message to producer topic
+        await producer.send({
+          topic: process.env.KAFKA_PRODUCER_TOPIC, // Make sure this env variable is set
+          messages: [{ value: msgValue }],
+        });
+
+        console.log("Published message to producer topic.");
       }catch(err){
         console.log("THis is the err--->",err)
       }
